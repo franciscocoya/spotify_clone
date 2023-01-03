3f4030b07658c030b00a78b6e7aaf8ec6bf74ac1
FranciscoCoya <kikocoya@gmail.com>
2023-01-03 21:22:52 +0100

    feat: login page skeleton and adding support to svg format

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>Página no encontrada - Spotify</title>
+      </Head>
+      <main>
+        <p>404 - Page Not Found</p>
+      </main>
+    </>
+  );
+}
