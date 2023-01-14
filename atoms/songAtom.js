128765d1ce099749ec45edb2d2c6e0dddeab575e
FranciscoCoya <kikocoya@gmail.com>
2023-01-14 21:17:48 +0100

    feat: add new atoms for the player management

--- atoms/songAtom.js
+++ atoms/songAtom.js
@@ -6,10 +6,27 @@ const songIsPlayingState = atom({
   default: false,
 });
 
+/* Path of song which is playing now */
+const currentPlayingSongState = atom({
+  key: 'currentPlayingSongState',
+  default: '/music/chilled-acoustic-indie-folk-ig-version-60s-9648.mp3',
+});
+
 /*Current song elapsed playing time */
 const elapsedPlayingState = atom({
   key: 'elapsedPlayingState',
   default: 0,
 });
 
-export { songIsPlayingState, elapsedPlayingState };
+/*Duration of the song which is playing now */
+const songDurationState = atom({
+  key: 'songDurationState',
+  default: 0,
+});
+
+export {
+  songIsPlayingState,
+  elapsedPlayingState,
+  currentPlayingSongState,
+  songDurationState,
+};
