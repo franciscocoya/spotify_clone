ba3c0dc2ac09d07739775f61e39bb59148e5e124
FranciscoCoya <kikocoya@gmail.com>
2023-01-15 16:13:30 +0100

    refactor: audio player progress bar animations

--- public/vendor/slider.js
+++ public/vendor/slider.js
@@ -1,36 +1,45 @@
-const setProgress = (value) => {
-  document.getElementById('progress-bar__marker').style.left =
-    value + (value > 0 && 'px');
-  document.getElementById('progress-bar-bg__elapsed').style.width =
-    value + 25 / 2 + 'px';
-};
+const whiteColor = '#FFFFFF';
+const empashisColor = '#1ED760';
 
-const moveProgressMarker = (e) => {
-  const progressValue = e.target.value;
-
-  if (progressValue <= 0) {
-    setProgress(0);
-  } else if (progressValue >= 100) {
-    setProgress(500 - 20 / 2);
-  } else if (progressValue >= 64 && progressValue <= 80) {
-    setProgress((500 * progressValue * 1.0) / 100.0 - 10);
-  } else if (progressValue > 80 && progressValue < 97) {
-    setProgress((500 * progressValue * 1.0) / 100.0 - 12);
-  } else if (progressValue >= 97 && progressValue < 100) {
-    setProgress((500 * progressValue * 1.0) / 100.0 - 15);
-  } else {
-    setProgress((500 * progressValue * 1.0) / 100.0 - 5);
-  }
-
-  return progressValue;
-};
-
-const hideProgressIcons = (e, hide) => {
+// Changes the styles of the elements in the progress bar when the cursor is positioned above or outside it.
+const hideProgressIcons = (
+  e,
+  progressElapsedId,
+  progressMarkerId,
+  currentValue,
+  hide
+) => {
   e.preventDefault();
-  document.getElementById('progress-bar__marker').style.opacity = hide ? 0 : 1;
-  document.getElementById('progress-bar-bg__elapsed').style.opacity = hide
-    ? 0.9
-    : 0;
+
+  hide
+    ? setProgressElementStyle(
+        0,
+        currentValue > 0 ? 0.9 : 0,
+        whiteColor,
+        progressElapsedId,
+        progressMarkerId
+      )
+    : setProgressElementStyle(
+        1,
+        1,
+        empashisColor,
+        progressElapsedId,
+        progressMarkerId
+      );
 };
 
-export { moveProgressMarker, hideProgressIcons };
+// Changes the progress bar style properties (Marker and elapsed time bar)
+const setProgressElementStyle = (
+  markerOpacity,
+  elapsedOpacity,
+  elapsedBackgroundColor,
+  progressElapsedId,
+  progressMarkerId
+) => {
+  document.getElementById(progressMarkerId).style.opacity = markerOpacity;
+  document.getElementById(progressElapsedId).style.opacity = elapsedOpacity;
+  document.getElementById(progressElapsedId).style.backgroundColor =
+    elapsedBackgroundColor;
+};
+
+export { hideProgressIcons };
