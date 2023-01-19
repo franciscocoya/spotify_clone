a29efdab4ed77fc6d7398d9fa5fdb8daa6b39852
FranciscoCoya <kikocoya@gmail.com>
2023-01-19 20:53:48 +0100

    feat: create recoil state for artist management purposes

--- atoms/artistAtom.js
+++ atoms/artistAtom.js
@@ -3,7 +3,10 @@ import { atom } from 'recoil';
 /* If user selects suffle option */
 const selectedArtistState = atom({
   key: 'selectedArtistState',
-  default: '',
+  default: {
+    shortlink: '',
+    id: '',
+  },
 });
 
 export { selectedArtistState };
