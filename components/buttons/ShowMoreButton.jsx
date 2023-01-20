0f0ee3b9c140aeb29742a8ed949a42fa25eb5d76
FranciscoCoya <kikocoya@gmail.com>
2023-01-20 16:17:05 +0100

    feat: create show more button

---
+++ components/buttons/ShowMoreButton.jsx
@@ -0,0 +1,27 @@
+import variables from '@styles/variables.module.scss';
+
+function ShowMoreButton({ children, ...props }) {
+  return (
+    <>
+      <button onClick={props.action} className='show-more-button'>
+        {children}
+      </button>
+      <style jsx>{`
+        .show-more-button{
+          font-size: 1rem;
+          font-weight: 500;
+          text-transform: uppercase;
+          color: ${variables.primaryColor};
+          background-color: transparent;
+          text-decoration: underline;
+        }
+
+        .show-more-button:hover{
+          color: ${variables.primaryColorEmphasis};
+        }
+    `}</style>
+    </>
+  )
+}
+
+export default ShowMoreButton;
