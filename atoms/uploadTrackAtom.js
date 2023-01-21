d46c1e44045f8fce9b94e399198e628bc83355d3
FranciscoCoya <kikocoya@gmail.com>
2023-01-21 20:52:13 +0100

    feat: create state management for upload track page

---
+++ atoms/uploadTrackAtom.js
@@ -0,0 +1,39 @@
+import { atom } from 'recoil';
+
+/* Track to upload file content */
+const trackToUploadDetailsState = atom({
+  key: 'selectedArtistState',
+  default: null,
+});
+
+/* Represents the cover of the track to upload */
+const coverToUploadDetailsState = atom({
+  key: 'coverToUploadDetailsState',
+  default: null,
+});
+
+/* Represents the cover of the track to upload */
+const trackUploadTitlePreviewState = atom({
+  key: 'trackUploadTitlePreviewState',
+  default: 'Title preview',
+});
+
+/* Represents the selected genre to apply for track */
+const selectedMusicGenreState = atom({
+  key: 'selectedMusicGenreState',
+  default: '',
+});
+
+/* Represents if selected track is explicit */
+const selectedTrackIsExplicit = atom({
+  key: 'selectedTrackIsExplicit',
+  default: false,
+});
+
+export {
+  trackToUploadDetailsState,
+  coverToUploadDetailsState,
+  trackUploadTitlePreviewState,
+  selectedMusicGenreState,
+  selectedTrackIsExplicit,
+};
