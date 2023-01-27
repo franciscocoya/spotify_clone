175298b96eefc8194b7a962938721f711ba19cc2
FranciscoCoya <kikocoya@gmail.com>
2023-01-27 20:57:26 +0100

    feat: create an artist account. backend calls

--- services/artistCrudService.js
+++ services/artistCrudService.js
@@ -2,6 +2,53 @@ import { artistRoute } from '@lib/apiRoutes';
 import axios from 'axios';
 
 /**
+ * Create an artist account.
+ * @param {*} payload Payload must have at least the following fields: name, email and password.
+ * The rest of the fields such as bio or page banner are optional.
+ * @returns
+ */
+const create = (payload, errorCallback) => {
+  try {
+    const errors = validateCreateParams(payload);
+    if (errors?.length > 0) {
+      errorCallback(errors);
+      console.log(errors);
+      return;
+    }
+
+    const { name, email, password, bio, pageBanner } = payload;
+
+    // const data = new FormData();
+    // data.append('name', name);
+    // data.append('email', email);
+    // data.append('password', password);
+    // data.append('bio', bio);
+    // data.append('banner', pageBanner);
+
+    //await axios.post(`${artistRoute}/new`, data);
+  } catch (error) {
+    console.error(`An error ocurred. ${error}`);
+  }
+};
+
+/**
+ * Auxiliar method of {@link create} to validate its params.
+ * @param {*} payload Params to validate.
+ * @returns An error list.
+ */
+const validateCreateParams = (payload) => {
+  const { name, email, password, bio, pageBanner } = payload;
+
+  let errors = new Map();
+
+  if (!name || name.replace(/\s/g, '') === '') {
+    errors.set('name', 'components.messages.form.error.empty');
+  }
+
+  return errors;
+};
+
+/**
  * Get all artist data by their id.
  * @param {*} id Artist id to search
  * @returns All artist information.
@@ -21,4 +68,4 @@ const getArtistNameById = async (id) => {
   return result.data.data.name;
 };
 
-export { getArtistById, getArtistNameById };
+export { getArtistById, getArtistNameById, create };
